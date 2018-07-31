3d85ced16d4b4a2f779b29ed3f66b3f2390df568
domates06 <35399633+Bot07@users.noreply.github.com>
2018-07-31 20:38:23 +0300

    Update and rename kivi.js to fb.js

---
+++ fb.js
@@ -0,0 +1,31 @@
+
+
+const embed = require('discord.js'); 
+
+
+
+const Discord = require('discord.js'); 
+
+
+
+const client = new Discord.Client(); 
+
+
+
+
+
+
+
+var prefix = 'fb!' 
+
+
+
+
+
+
+
+client.on("ready", () => {client.user.setGame('Yapım Aşamasında' , 'https://twitch.tv/FalcıBeta') 
+
+
+
+}); 
